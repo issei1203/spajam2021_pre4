8bc6fa695421915cd20b8e07b0f3bc33db767cf4
issei_itagaki <i-itagaki@atware.co.jp>
2021-10-09 21:26:33 +0900

    build.gradle修正

--- src/main/java/com/spajam2021_pre4/spajam2021_pre4/Controller/Spjcontroller.java
+++ src/main/java/com/spajam2021_pre4/spajam2021_pre4/Controller/Spjcontroller.java
@@ -4,11 +4,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Spjcontroller {
     @GetMapping("/")
     ResponseEntity<String> hello(){
         return new ResponseEntity<String>("Hello! This is spajam2021_pre4", HttpStatus.OK);
+    }
+
+    @PostMapping("/arrival")
+    ResponseEntity<String> getPosition(@RequestParam String url){
+        return new ResponseEntity<String>(url, HttpStatus.OK);
     }
 }
---
+++ src/test/java/com/spajam2021_pre4/spajam2021_pre4/Controller/SpjcontrollerTest.java
@@ -0,0 +1,26 @@
+package com.spajam2021_pre4.spajam2021_pre4.Controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SpjcontrollerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void test_shouldGetPosition() throws Exception {
+        MvcResult result = mockMvc.perform(post("/arrival").param("url","sample.com")).andExpect(status().isOk()).andReturn();
+        assertEquals("sample.com",result.getResponse().getContentAsString());
+    }
+}
